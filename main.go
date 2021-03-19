37d7149876f4c74d5c912b29f181521f191351ef
Ashish Ghosh <ashish21ghosh@gmail.com>
2021-03-19 11:13:42 +0530

    added string data type

--- main.go
+++ main.go
@@ -22,9 +22,17 @@ type (
 		destClient *redis.Client
 		ctx        context.Context
 		key        string
-		keyType    string
+		keyType    RedisType
 		err        error
 	}
+
+	RedisType string
+)
+
+const (
+	RedisHash   RedisType = "hash"
+	RedisString RedisType = "string"
+	RedisNone   RedisType = "none"
 )
 
 // ParseCliArgs - parses command line arguments
@@ -82,8 +90,8 @@ func (a *AppCtx) GetRedisKeyType() *AppCtx {
 		return a
 	}
 
-	a.keyType = val.(string)
-	if a.keyType == "none" {
+	a.keyType = RedisType(val.(string))
+	if a.keyType == RedisNone {
 		a.err = fmt.Errorf("GetRedisKeyType: key %s does not exist", a.key)
 	}
 	return a
@@ -94,13 +102,13 @@ func (a *AppCtx) CopyHMData() *AppCtx {
 	if a.err != nil {
 		return a
 	}
-	if a.keyType != "hash" {
+	if a.keyType != RedisHash {
 		a.err = fmt.Errorf("CopyHMData: invalid data type %s", a.keyType)
 	}
 	// query source db
 	iter := a.srcClient.HScan(a.ctx, a.key, 0, "", 1000).Iterator()
 	if err := iter.Err(); err != nil {
-		a.err = fmt.Errorf("CopyRedisData: %v", err)
+		a.err = fmt.Errorf("CopyHMData: %v", err)
 		return a
 	}
 
@@ -113,14 +121,61 @@ func (a *AppCtx) CopyHMData() *AppCtx {
 	// insert into dest
 	res, err := a.destClient.HMSet(a.ctx, a.key, args...).Result()
 	if err != nil {
-		a.err = fmt.Errorf("CopyRedisData: %v", err)
+		a.err = fmt.Errorf("CopyHMData: %v", err)
 		return a
 	}
 	if !res {
-		a.err = fmt.Errorf("CopyRedisData: %v", err)
+		a.err = fmt.Errorf("CopyHMData: could not save to dest, response: %v", res)
 		return a
 	}
 	return a
+}
+
+// CopyStringData - copy data from redis
+func (a *AppCtx) CopyStringData() *AppCtx {
+	if a.err != nil {
+		return a
+	}
+	if a.keyType != RedisString {
+		a.err = fmt.Errorf("CopyStringData: invalid data type %s", a.keyType)
+	}
+	// query source db
+	srcVal, err := a.srcClient.Get(a.ctx, a.key).Result()
+	if err != nil {
+		a.err = fmt.Errorf("CopyStringData: error while fetching data: %v", err)
+		return a
+	}
+
+	// insert into dest
+	res, err := a.destClient.Set(a.ctx, a.key, srcVal, -1).Result()
+	if err != nil {
+		a.err = fmt.Errorf("CopyRedisData: %v", err)
+		return a
+	}
+	if res != "OK" {
+		a.err = fmt.Errorf("CopyStringData: could not save to dest, response: %s", res)
+		return a
+	}
+	return a
+}
+
+// Copy - copy data from source to dest
+func (a *AppCtx) Copy() (err error) {
+	if a.err != nil {
+		return a.Error()
+	}
+	if a.keyType == RedisHash {
+		a = a.CopyHMData()
+	} else if a.keyType == RedisString {
+		a = a.CopyStringData()
+	} else {
+		return fmt.Errorf("Unsupported redis key type: %s", string(a.keyType))
+	}
+	err = a.Error()
+	if err != nil {
+		return
+	}
+	return nil
 }
 
 // Error - get error
@@ -129,9 +184,11 @@ func (a *AppCtx) Error() error {
 }
 
 func main() {
-	val := ParseCliArgs().GetAppContext().GetRedisKeyType().CopyHMData()
-	err := val.Error()
+	appCtx := ParseCliArgs().GetAppContext().GetRedisKeyType()
+	err := appCtx.Copy()
 	if err != nil {
 		fmt.Println("ERROR:", err)
+	} else {
+		fmt.Println("Done")
 	}
 }
